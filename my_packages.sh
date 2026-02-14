23a86360f4def7e20ba6de3fa9db8bb14c81ad09
Ayu <79645007+Ace-c@users.noreply.github.com>
2026-02-14 16:51:46 +0530

    Update my_packages.sh

--- my_packages.sh
+++ my_packages.sh
@@ -3,18 +3,17 @@
 sudo apt update
 essential_packages=(
     "build-essential" #meta-package that installs everything you need to compile software from source
-    "git"  #kaun nahi janta issee
+    "git"  #free and open-source tool for tracking changes in computer files
     "wget"  #Better for simple file downloads and recursive "web-scraping."
     "curl"  #generally more powerful for data transfer
-    "htop"  #monitoring in real time
+    "htop"  #system monitoring in real time
     "dstat"   #Tool that combines the power of vmstat, iostat, and ifstat
     "ripgrep" #incredibly fast replacement for grep
     "sed"  #used to perform basic text transformations
     "gawk"  #designed for processing and analyzing text files that are structured in rows and columns
     "fd-find"  #faster and more intuitive alternative to the traditional find command
     "fzf"  #fuzzy finder
-    "neovim"  #kaun nahi janta isse
-    "tmux"   #terminal multiplexer
+    "neovim"  #Open source text editor, an fork of vim
     "lnav"   #Log-file navigator
     "net-tools"   #collection of legacy networking utilities like ifconfig, route, and netstat
     "ufw"   #managing complex firewall rules, making it easy to block or allow specific traffic.
@@ -23,13 +22,13 @@ essential_packages=(
     "nmap"  #used for security auditing and discovering what devices and ports are active on a network.
     "tcpdump"   #packet sniffer, It lets you capture and inspect the actual data packets traveling over your network
     "traceroute"   #diagnostic tool that shows the "path" a packet takes across the internet to reach a destination, helping you find where a connection is failing
-    "openssh-server" #SSH
+    "openssh-server" #open-source connectivity tool for remote login, using ssh
     "rsync"  #copying and synchronizing files between folders or different servers
-    "firejail"   #kamre mai bnd krke rakhta hai, sans leke ke liye bs ek ched khula rakhta hai
+    "firejail"   #sandbox program for Linux that enhances security by isolating applications in their own restricted environments
     "gnupg"   #used for encrypting and signing data
     "rkhunter"  #It scans your system for hidden malware, backdoors and rootkits
     "lynis"  #It scans your entire system and provides a "Hardening Index" score along with specific tips
-    "inxi"   # It used to display hardware, driver, kernel, and system-level data consise and clearly
+    "inxi"   #It provides summary of your system hardware, drivers, kernel, and system-level data
 )
 
 for package in "${essential_packages[@]}"; do
@@ -94,35 +93,19 @@ echo "Installation complete!"
 
 
 # Manual installation of packages
-## Vivaldi-Broswer
-if dpkg -s vivaldi-stable &>/dev/null;then
-    echo "Vivaldi is already installed! skipping..."
+## Cryptomator
+echo "Installing cryptomator..."
+if dpkg -s cryptomator &>/dev/null; then
+    echo "Cryptomator already installed - skipping"
 else
-curl -fsSL https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/vivaldi.gpg
-cat <<EOF | sudo tee /etc/apt/sources.list.d/vivaldi.sources
-Types: deb
-URIs: https://repo.vivaldi.com/stable/deb/
-Suites: stable
-Components: main
-Architectures: amd64 arm64 armhf
-Signed-By: /usr/share/keyrings/vivaldi.gpg
-EOF
-sudo rm -f /etc/apt/sources.list.d/vivaldi.list
-sudo apt update
-sudo apt install vivaldi-stable -y
-fi
+VER=$(curl -s https://api.github.com/repos/cryptomator/cryptomator/releases/latest | grep -oP '"tag_name": "\K[^"]+')
+wget "https://github.com/cryptomator/cryptomator/releases/download/$VER/cryptomator_${VER}-0ppa1_amd64.deb"
+sudo dpkg -i "cryptomator_${VER}-0ppa1_amd64.deb"
+sudo apt install -f -y #fix broken
+rm -f "cryptomator_${VER}-0ppa1_amd64.deb"
+echo "Cryptomator installed"
+fi 
 
-## Mega-sync
-echo "Installing mega-sync package..."
-wget https://mega.nz/linux/repo/Debian_13/amd64/megasync-Debian_13_amd64.deb && sudo apt install "$PWD/megasync-Debian_13_amd64.deb"
-sudo apt install -f -y
-rm -f megasync-Debian_13_amd64.deb
-echo "Megasync installed"
-
-wget https://mega.nz/linux/repo/Debian_13/amd64/nautilus-megasync-Debian_13_amd64.deb && sudo apt install "$PWD/nautilus-megasync-Debian_13_amd64.deb"
-sudo apt install -f -y
-rm -f nautilus-megasync-Debian_13_amd64.deb
-echo "Megasync nautilus integration added"
 
 ## Google Antigravity 
 echo "Installing Google-Antigravity IDE..."
@@ -140,6 +123,7 @@ sudo apt install antigravity -y
 echo "Google Antigravity Installed"
 fi 
 
+
 ## Cursor IDE
 echo "Installing Cursor IDE..."
 if dpkg -s cursor &>/dev/null; then
@@ -152,12 +136,42 @@ sudo apt install cursor -y
 echo "Cursor IDE Installed"
 fi
 
+
+## Mega-sync
+echo "Installing mega-sync package..."
+if dpkg -s megasync &>/dev/null; then
+    echo "mega-sync already installed -- Skipping.."
+else
+wget https://mega.nz/linux/repo/Debian_13/amd64/megasync-Debian_13_amd64.deb && sudo apt install "$PWD/megasync-Debian_13_amd64.deb"
+wget https://mega.nz/linux/repo/Debian_13/amd64/nautilus-megasync-Debian_13_amd64.deb && sudo apt install "$PWD/nautilus-megasync-Debian_13_amd64.deb"
+sudo apt install -f -y
+rm -f megasync-Debian_13_amd64.deb
+rm -f nautilus-megasync-Debian_13_amd64.deb
+echo "Megasync installed"
+fi
+
+
+## Anytype
+echo "Installing anytype desktop"
+if dpkg -s anytype &>/dev/null; then
+    echo "Anytype is installed -- skipping"
+else
+VER=$(curl -s https://api.github.com/repos/anyproto/anytype-ts/releases/latest | grep -oP '"tag_name": "v\K[^"]+')
+wget "https://github.com/anyproto/anytype-ts/releases/download/v${VER}/anytype_${VER}_amd64.deb"
+sudo dpkg -i "anytype_${VER}_amd64.deb"
+sudo apt install -f -y
+rm -f "anytype_${VER}_amd64.deb"
+echo "Anytype installed"
+fi
+
+
 ## Free Download Manager
 echo "Installing freedownloadmanager"
 wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb && sudo apt install "$PWD/freedownloadmanager.deb"
 sudo apt install -f -y
 rm -f freedownloadmanager.deb
 echo " FDM installed"
+
 
 ## Obsidian
 echo "Installing obsidian..."
@@ -179,6 +193,7 @@ else
     echo "Error: Could not find the download URL"
 fi
 
+
 ## Opencomic 
 echo "Installing opencomic reader..."
 OPENCOMIC_URL=$(curl -s https://api.github.com/repos/ollm/OpenComic/releases/latest | grep "browser_download_url.*_amd64.deb" | cut -d '"' -f 4)
@@ -199,24 +214,31 @@ else
     echo "Error: Could not find the OpenComic download URL."
 fi
 
-## Cryptomator
-echo "Installing cryptomator..."
-if dpkg -s cryptomator &>/dev/null; then
-    echo "Cryptomator already installed - skipping"
-else
-VER=$(curl -s https://api.github.com/repos/cryptomator/cryptomator/releases/latest | grep -oP '"tag_name": "\K[^"]+')
-wget "https://github.com/cryptomator/cryptomator/releases/download/$VER/cryptomator_${VER}-0ppa1_amd64.deb"
-sudo dpkg -i "cryptomator_${VER}-0ppa1_amd64.deb"
-sudo apt install -f -y #fix broken
-rm -f "cryptomator_${VER}-0ppa1_amd64.deb"
-echo "Cryptomator installed"
-fi 
 
 ## Github-Desktop
 echo "Installing github-desktop..."
 wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/mwt-desktop.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
 sudo apt update && sudo apt install github-desktop -y
+
+
+## Vivaldi-Broswer
+if dpkg -s vivaldi-stable &>/dev/null;then
+    echo "Vivaldi is already installed! skipping..."
+else
+curl -fsSL https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/vivaldi.gpg
+cat <<EOF | sudo tee /etc/apt/sources.list.d/vivaldi.sources
+Types: deb
+URIs: https://repo.vivaldi.com/stable/deb/
+Suites: stable
+Components: main
+Architectures: amd64 arm64 armhf
+Signed-By: /usr/share/keyrings/vivaldi.gpg
+EOF
+sudo rm -f /etc/apt/sources.list.d/vivaldi.list
+sudo apt update
+sudo apt install vivaldi-stable -y
+fi
 
 
 echo "Required Packages Installed!"
