e74b50470ebcc9aa3a1625501cf9479a123bd58a
Ayu <79645007+Ace-c@users.noreply.github.com>
2026-07-16 06:00:15 +0530

    Update dark_style.sh

--- .config/scripts/dark_style.sh
+++ .config/scripts/dark_style.sh
@@ -2,21 +2,10 @@
 
 KV_CONF="$HOME/.config/Kvantum/kvantum.kvconfig"
 
-
-DARK_KVANTUM="kvantum"
 LIGHT_KVANTUM="AustralAzure"
+DARK_KVANTUM="kvantum"
 
-apply_dark() {
-    echo "Applying GTK Dark Theme..."
-    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    gsettings set org.gnome.desktop.interface gtk-theme "slate-Dark-solid"
-    gsettings set org.gnome.shell.extensions.user-theme name 'Marble-blue-dark'
 
-    echo "Applying qt Dark Theme..."
-    sed -i "s/^theme=.*/theme=$DARK_KVANTUM/" "$KV_CONF"
-    
-    gsettings set org.gnome.desktop.background picture-uri-dark "$HOME/Pictures/wallpapers/Everblush_Mike_erskine.jpg"
-}
 
 apply_light() {
     echo "Applying GTK Light Theme..."
@@ -31,12 +20,26 @@ apply_light() {
 }
 
 
+apply_dark() {
+    echo "Applying GTK Dark Theme..."
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    gsettings set org.gnome.desktop.interface gtk-theme "slate-Dark-solid"
+    gsettings set org.gnome.shell.extensions.user-theme name 'Marble-blue-dark'
+
+    echo "Applying qt Dark Theme..."
+    sed -i "s/^theme=.*/theme=$DARK_KVANTUM/" "$KV_CONF"
+    
+    gsettings set org.gnome.desktop.background picture-uri-dark "$HOME/Pictures/wallpapers/Everblush_Mike_erskine.jpg"
+}
+
+
+
 case "$1" in
-    -d|--dark)
-        apply_dark
-        ;;
     -l|--light)
         apply_light
+        ;;
+    -d|--dark)
+        apply_dark
         ;;
     *)
         echo "Usage: ./dark_syle.sh [-d (dark) | -l (light)]"
